5a7733af6279ee2c6ba8b17ee8a54a4427e6431b
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-15 09:03:34 +0000

    update

--- src/utils/mt5.js
+++ src/utils/mt5.js
@@ -1,18 +1,108 @@
 const Net = require('net');
 
-const port = 77;
-const host = "localhost";
+class Mt5 {
+	
+	constructor() {
+		this.port = 77;
+		this.host = "localhost";
+		this.client = new Net.Socket();
+		this.closeData = { "MSG" : "ORDER_CLOSE", "TICKET": null};
+		this.orderData = {"MSG":"ORDER_SEND","SYMBOL":"EURUSD","VOLUME": 0,"TYPE":"ORDER_TYPE_BUY"};
 
-const client = new Net.Socket();
-client.connect(port,host, function() {
-	client.write('{"MSG":"QUOTE","SYMBOL":"EURUSD"}' + '\r\n');
+	}
+	
+	connect() {
+		try{
+			this.client.connect(this.port, this.host);
+		}catch (err){
+			console.log(error);
+			return 0;
+		}
 
-});
+		return 1
+	}
 
-client.on('data', function(chunk){
-	console.log('fetching data');
-	console.log(`${chunk.toString()}`);
-	client.end();
-});
+	getDetails(symbol){
+		let response_data = null;
+		try{
+			this.client.write(`{"MSG":"QUOTE","SYMBOL": ${symbol}}` + '\r\n');
+		}catch (err) {
+			console.log(error);
+			return response_data;
+		}
+		
+		try{
+			this.client.on('data', function(chunk, data){
+				response_data = JSON.parse(chunk.toString());
+				console.log(data);
+			});
+		}catch (err) {
+			console.log(err);
+			return response_data;
+		}
+		
+		return response_data;
+	}
+	
+	openOrder(data) {
+		let response_data = null;
+		this.orderData["SYMBOL"] = data["symbol"];
+		this.orderData["VOLUME"] = data["volume"];
+		this.orderData["TYPE"] = `ORDER_TYPE_${data["image"]}`;
+		try {
+			this.client.write(this.orderData);
+		}catch(err) {
+			console.log(err);
+			return 0;
+		}
+
+		try {
+			this.client.on('data', function(chunk, response_data){
+				response_data = JSON.parse(chunk.toString());
+			});
+		}catch (err) {
+			console.log(err);
+			return response_data;
+		}
+		return response_data;
+	}
+
+	closeOrder(data) {
+		let response_data = null;
+		this.closeData["TICKET"] = data["ticket"];
+		try {
+			this.client.write(this.closeData);
+		}catch(err) {
+			console.log(err);
+			return 0;
+		}
+
+		try {
+			this.client.on('data', function(chunk, response_data){
+				response_data = JSON.parse(chunk.toString());
+			});
+		}catch (err) {
+			console.log(err);
+			return response_data;
+		}
+		return response_data;
+
+		
+	}
+	
+
+	exit(){
+		try {
+			this.client.end();
+		}catch (err) {
+			console.log(err);
+		}
+
+	}
+	
+
+}
+
+module.exports = Mt5;
 
 //client.exit();
