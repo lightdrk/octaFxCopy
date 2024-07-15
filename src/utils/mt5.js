e9fe4b4dc830db0676f1c88aebfeedd91f823b60
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-15 06:19:30 +0000

    update

--- src/utils/mt5.js
+++ src/utils/mt5.js
@@ -5,11 +5,14 @@ const host = "localhost";
 
 const client = new Net.Socket();
 client.connect(port,host, function() {
-	client.write('{"MSG":"QUOTE","SYMBOL":"EURUSD"}\n');
+	client.write('{"MSG":"QUOTE","SYMBOL":"EURUSD"}' + '\r\n');
 
 });
 
 client.on('data', function(chunk){
+	console.log('fetching data');
 	console.log(`${chunk.toString()}`);
 	client.end();
 });
+
+client.close();
