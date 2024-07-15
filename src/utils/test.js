b00cbfd1f00a6191cd4217e0c449a283957f8cb2
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-15 15:42:26 +0000

    update

--- src/utils/test.js
+++ src/utils/test.js
@@ -6,8 +6,10 @@ let details = null;
 if (isConnected){
 	details = m.getDetails("EURUSD");
 }
+console.log(dtails);
 let isOpen = null;
 if (details){
+	console.log("in")
 	isOpen = m.openOrder({"symbol": "EURUSD", "volume": 0.5, "image": "SELL"});
 }
 
