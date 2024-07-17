5e55cdf11f85f6be1a992a70106f2d1a484f8cf8
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-17 07:25:10 +0000

    update

--- src/utils/test.js
+++ src/utils/test.js
@@ -5,10 +5,6 @@ async function test(){
 	let isConnected = m.connect();
 	let details = null;
 	console.log(isConnected);
-	if (isConnected){
-		details = await m.getDetails("EURUSD");
-	}
-	console.log('details-->',details);
 	let isOpen = null;
 	console.log("in")
 	isOpen = await m.openOrder({"symbol": "EURUSD", "volume": 0.5, "image": "SELL"});
