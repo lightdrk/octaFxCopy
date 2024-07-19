7d49b5d9f6b87190c4fb2c85e78ba318c4fbbe22
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-19 13:01:16 +0000

    update

--- src/test_filter.js
+++ src/test_filter.js
@@ -11,10 +11,14 @@ const { compare } = require('./utils/compare');
 		args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized'],
 	});
 	const octafx = new OctaFx(browser);
-	await octafx.openPage(["27366825"]);
+	await octafx.openPage(["27366825", "21139687","20945089","22737135","27366823"]);
 	const data = await octafx.dataRetr();
 	console.log('data -----> ',data)
-	const oldData = [data[1]];
+	const oldData = data;
+	for (let x of oldData){
+		x["ticket"] = 1112323;
+	}
+	console.log(oldData);
 	const t = filter(oldData, data);
 	const c = compare(data, oldData);
 	console.log('filter --->',t);
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -18,11 +18,10 @@ function filter(old, newData) {
 	const update = [];
 	for (let x of newData){
 		let time_duration = x.duration;
+		console.log(time_duration);
 		let checker = toCheck(time_duration);
 		if (checker){
 			var isIn = null;
-			// issue here is if old data has one data that is similar to all the data in new taht's issue
-			// if their is not time;
 			for (let y of old){
 				if (y.open_time === x.open_time && y.volume === x.volume && y.symbol === x.symbol && y.type === x.type){
 					isIn = 1;
