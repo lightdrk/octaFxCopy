f1d167992502cffb48ca7b9f4ac5fcf78d82394a
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-19 03:59:52 +0000

    update

--- src/test-main.js
+++ src/test-main.js
@@ -8,11 +8,6 @@ const { compare } = require('./utils/compare');
 const { sendMessages } = require('./utils/notify');
 
 //TODO: store data , check this file, issue here with getting ticket check filter it has ticket new data which can cause it to always be yes for data, 
-require('dotenv').config();
-
-const config = process.env;
-console.log(config);
-const fx = new OneFx(config);
 let old = null;
 try{
 	old = fs.readFileSync("cached.json",'utf-8');
