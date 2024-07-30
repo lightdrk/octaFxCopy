6b470c4c556bd0a9efc5c3f655f8a55ef6eb8469
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-30 08:26:36 +0000

    updated compare as it had issues

--- src/utils/compare.js
+++ src/utils/compare.js
@@ -1,15 +1,20 @@
 function compare(old, newData) {
 	const diff = [];
+	const newData_copy = [...newData];
+	console.log(newData_copy);
 	for (let x of old){
 		var isIn = null;
-		for (let y of newData){
-			console.log(`${x.open_time} === ${y.open_time}`, x.open_time === y.open_time);
-			console.log(`${x.volume} === ${y.volume}`, x.volume === y.volume);
-			console.log(`${x.symbol} === ${y.symbol}`, x.symbol === y.symbol);
-			console.log(`${x.type} === ${y.type}`,  x.type === y.type);
-			if (x.open_time === y.open_time && x.volume === y.volume && x.symbol === y.symbol && x.type === y.type){
-				isIn = 1;
-				break;
+		for (let y = 0; y < newData_copy.length; y++){
+			if (newData_copy[y] != undefined){
+				console.log(`${x.open_time} === ${newData_copy[y].open_time}`, x.open_time === newData_copy[y].open_time);
+				console.log(`${x.volume} === ${newData_copy[y].volume}`, x.volume === newData_copy[y].volume);
+				console.log(`${x.symbol} === ${newData_copy[y].symbol}`, x.symbol === newData_copy[y].symbol);
+				console.log(`${x.type} === ${newData_copy[y].type}`,  x.type === newData_copy[y].type);
+				if (x.open_time === newData_copy[y].open_time && x.volume === newData_copy[y].volume && x.symbol === newData_copy[y].symbol && x.type === newData_copy[y].type){
+					isIn = 1;
+					delete newData_copy[y];
+					break;
+				}
 			}
 		}
 		if (isIn == null){
@@ -19,6 +24,4 @@ function compare(old, newData) {
 	return diff;
 
 }
-
-
 module.exports.compare = compare;
