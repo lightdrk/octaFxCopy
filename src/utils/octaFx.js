9855df353e8fe249eaaabc5109643fbb24b9bf21
mohit rawat <mohitrawatssj4@gmail.com>
2024-07-31 11:07:00 +0000

    updated octaFx for better error handling , if data is not found it just loops over

--- src/utils/octaFx.js
+++ src/utils/octaFx.js
@@ -33,79 +33,48 @@ class OctaFx {
 	}
 
 	async dataRetr(refresh_time) {
-		console.log(this.pages);
 		const data =[];
 		const call = {'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iNCIgZmlsbD0iI0U3RjFGRiIvPgo8cGF0aCBkPSJNMTcgMTZMMTcgN0w4IDciIHN0cm9rZT0iIzBENkZGQiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPHJlY3QgeD0iMTUuODcyMSIgeT0iNi40OTczOCIgd2lkdGg9IjIuMjk0NDYiIGhlaWdodD0iMTMuMjkxNSIgcng9IjEuMTQ3MjMiIHRyYW5zZm9ybT0icm90YXRlKDQ1IDE1Ljg3MjEgNi40OTczOCkiIGZpbGw9IiMwRDZGRkIiLz4KPC9zdmc+Cg==': 'BUY', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iNCIgZmlsbD0iI0ZGRUFDQyIvPgo8cGF0aCBkPSJNOCAxNkwxNyAxNkwxNyA3IiBzdHJva2U9IiNGRjk0MDAiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+CjxyZWN0IHg9IjE2LjcwMjYiIHk9IjE1LjAzMDYiIHdpZHRoPSIxLjI3NTM4IiBoZWlnaHQ9IjEyLjE4MSIgcng9IjAuNjM3NjkyIiB0cmFuc2Zvcm09InJvdGF0ZSgxMzUgMTYuNzAyNiAxNS4wMzA2KSIgZmlsbD0iI0ZGOTQwMCIgc3Ryb2tlPSIjRkY5NDAwIi8+Cjwvc3ZnPgo=': 'SELL'};
-		console.log(this.pages);
-		for (let page of this.pages) {
-			console.log('re-loading');
+		let index = 0;
+		while (index < this.pages.length) {
+			let page = this.pages[index];
 			try {
+				console.log('re-loading');
 				await page.reload({waitUntil: 'load'});
-			}catch (err){
-				this.ERROR = {'error':err, 'err_type': 'critical'};
-				console.log('error reloading page \nWaiting for 10sec to reload');
-				await new Promise(resolve => setTimeout(resolve,10000));
-			}
 
-			if (this.ERROR){
-				try{
-					await page.reload({waitUntil: 'load'});
-				}catch (err){
-					this.ERROR = {'error':err, 'err_type': 'critical'};
-				}
-			}
-			
-			try {
 				await page.screenshot({path: 'octafx.png'});
-			}catch (err){
-				this.ERROR = {'error':err, 'err_type': 'normal'};
-			}
 	
-			let openOrder = null;
-			try{
-				openOrder = await page.$$('button[class="ct-button text-button ct-tab _secondary _flat"]');
-			}catch (err){
-				this.Error = {'error': err, 'err_type': 'critical'};
-			}
+				let openOrder = await page.$$('button[class="ct-button text-button ct-tab _secondary _flat"]');
+				let numberOfOpenOrders = null;
+				if (openOrder){
+					numberOfOpenOrders = await page.evaluate(el => el.innerText, openOrder[0]);
+				}else {
+					this.Error = {'error': 'openOrder html not found', 'err_type': 'critical'};
+					await page.screenshot({path: 'openOrder.png'});
+				}
 
-			let numberOfOpenOrders 
-			if (openOrder){
-				numberOfOpenOrders = await page.evaluate(el => el.innerText, openOrder[0]);
-			}else {
-				this.Error = {'error': 'openOrder html not found', 'err_type': 'critical'};
-				await page.Screenshot({path: 'openOrder.png'});
-			}
-
-			if (this.isOpen(numberOfOpenOrders)){
-
-				await openOrder[0].click();
-				const divList = await page.$$('div[class="history-table"]');
-				let openOrderDiv = null;
-				for (let x of divList) {
-					const visible = await x.isVisible();
-					if (visible){
-						openOrderDiv = x;
-						break;
+				if (this.isOpen(numberOfOpenOrders)){
+					await openOrder[0].click();
+					const divList = await page.$$('div[class="history-table"]');
+					let openOrderDiv = null;
+					for (let x of divList) {
+						const visible = await x.isVisible();
+						if (visible){
+							openOrderDiv = x;
+							break;
+						}
 					}
 				}
 
-				try {
-					await openOrderDiv.waitForSelector('img',{timeout:3000});
-					await openOrderDiv.waitForSelector('div[class="history-table__volume"]',{timeout:3000});
-				}catch (err){
-					console.log("no updates\n waiting 100sec to reload");
-					await new Promise(resolve => setTimeout(resolve,100000));
-					break;
-					//console.error(err);
-				}
+				await openOrderDiv.waitForSelector('img',{timeout:3000});
+				await openOrderDiv.waitForSelector('div[class="history-table__volume"]',{timeout:3000});
 
 				const images = await openOrderDiv.$$('img');
 				const volume = await openOrderDiv.$$('div[class="history-table__volume"]');
 				const currency = await openOrderDiv.$$('div[class="history-table__currency"]');
 				const duration = await openOrderDiv.$$('div[class="history-table__row-section _duration"]');
 				const open_time = await openOrderDiv.$$('div[class="history-table__row-section _time"]');
-				
-
+					
 				for (let n = 0; n<volume.length; n++){
 					const vlm = await page.evaluate( el => el.innerText, volume[n] );
 					const img = await page.evaluate( el => el.src, images[n] );
@@ -114,8 +83,12 @@ class OctaFx {
 					const opt = await page.evaluate( el => el.innerText, open_time[n] );
 					data.push({volume: vlm, symbol: cur, type: call[img], duration: dur, open_time: opt});
 				}
-
+			}catch (err){
+				console.log(err);
+				await new Promise(resolve => setTimeout(resolve,refresh_time));
+				continue;
 			}
+			index ++;
 			await new Promise(resolve => setTimeout(resolve,refresh_time));	
 
 		}
