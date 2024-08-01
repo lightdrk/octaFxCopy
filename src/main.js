02ace9c16740c1f118fb92504fd3687d6015deb4
mohit rawat <mohitrawatssj4@gmail.com>
2024-08-01 09:44:50 +0000

    update it for main

--- src/main.js
+++ src/main.js
@@ -51,45 +51,50 @@ try{
 	const mt = new mt5();
 	let isConnected = mt.connect();
 	while (isConnected){
-		const newData = await octafx.dataRetr(data_yaml.refresh_time);//[{'symbol': 'GBPUSD', 'volume': '0.2', 'image': 'Sell'}]
-		console.log('newData -->',newData);
-		if (old === null || old.length == 0){
-			creation = filter(old,newData);
-		}else {
-			// comparing old and new 
-			const remove = compare(old,newData);
-			console.log('remove -->',remove);
-			for (let ticket of remove){
-				console.log(ticket);
-				let isClosed = await mt.closeOrder(ticket);
-				console.log(`****Closing order ${isClosed} ******`);
-				if (isClosed){
-					for (let i = 0; i < old.length; i++){
-						if (old[i].ticket === ticket.ticket){
-							old.splice(i,1);
-						}	
-					}
-					await sendMessages(data_yaml.telegram, `Removed ${JSON.stringify(isClosed)}`);
-				}else{
-					await sendMessages(data_yaml.telegram, `Unable to Remove ${JSON.stringify(remove)}`);
-				}
-			}
-			creation = filter(old,newData); 
-		}
-
-		//opening position
-		for (let index=0; index < creation.length; index++){
-			console.log('open postiion -->',creation[index]);
-			let isOpen = await mt.openOrder(creation[index]);
-			creation[index]["ticket"] = isOpen["ORDER"]; 
-			if (isOpen){
-				await sendMessages(data_yaml.telegram, `Position opened ${JSON.stringify(isOpen)}`);
+		const [newData, error ]= await octafx.dataRetr(data_yaml.refresh_time);//[{'symbol': 'GBPUSD', 'volume': '0.2', 'image': 'Sell'}]
+		if (error != null && error.details === 'blocked'){
+			console.log('newData -->',newData);
+			if (old === null || old.length == 0){
+				creation = filter(old,newData);
 			}else {
-				await sendMessages(data_yaml.telegram, `Unable to open position ${JSON.stringify(create)}`);
+				// comparing old and new 
+				const remove = compare(old,newData);
+				console.log('remove -->',remove);
+				for (let ticket of remove){
+					console.log(ticket);
+					let isClosed = await mt.closeOrder(ticket);
+					console.log(`****Closing order ${isClosed} ******`);
+					if (isClosed){
+						for (let i = 0; i < old.length; i++){
+							if (old[i].ticket === ticket.ticket){
+								old.splice(i,1);
+							}	
+						}
+						await sendMessages(data_yaml.telegram, `Removed ${JSON.stringify(isClosed)}`);
+					}else{
+						await sendMessages(data_yaml.telegram, `Unable to Remove ${JSON.stringify(remove)}`);
+					}
+				}
+				creation = filter(old,newData); 
 			}
-			old.push(creation[index]);
-		}
-		console.log('Updating cache ....', old);
-		fs.writeFileSync("cached.json",JSON.stringify(old),"utf-8");
+
+			//opening position
+			for (let index=0; index < creation.length; index++){
+				console.log('open postiion -->',creation[index]);
+				let isOpen = await mt.openOrder(creation[index]);
+				creation[index]["ticket"] = isOpen["ORDER"]; 
+				if (isOpen){
+					await sendMessages(data_yaml.telegram, `Position opened ${JSON.stringify(isOpen)}`);
+				}else {
+					await sendMessages(data_yaml.telegram, `Unable to open position ${JSON.stringify(create)}`);
+				}
+				old.push(creation[index]);
+			}
+			console.log('Updating cache ....', old);
+			fs.writeFileSync("cached.json",JSON.stringify(old),"utf-8");
+		}else {
+			await sendMessages(data_yaml.telegram,`Blocked waiting for 5 mins to re-run`)
+			await new Promise(resolve => setTimeout(resolve,300000));
+		}		
 	}
 })();
